276929911708dc92a9f77e45f6f4a7e97f144a07
rishiraj007 <rishiraj@mounttalent.com>
2023-06-20 16:06:33 +0530

    [Prabhu]Add: DS Practice Problem

---
+++ DSPracticeProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DSPracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data Structure Practice Problem!");
+        }
+    }
+}
