ae091c1ee37674676b32478c1ebb38bb75bff70a
rishiraj007 <rishiraj@mounttalent.com>
2023-06-20 16:17:20 +0530

    [Prabhu]Add: create generic linked list

--- DSPracticeProblem/Program.cs
+++ DSPracticeProblem/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace DSPracticeProblem
+﻿using System.Collections.Generic;
+
+namespace DSPracticeProblem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Data Structure Practice Problem!");
+            LinkedList<int> list = new LinkedList<int>();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nSelect the operation to perform \n1.Create Linked List \n2.Display \n3.Exit");
+                int options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        list.Add(56);
+                        list.Add(30);
+                        list.Add(70);
+                        break;
+                    case 2:
+                        list.Display();
+                        break;
+                    default:
+                        flag=false;
+                        break;
+                }
+            }
         }
     }
 }
